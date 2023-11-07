77aaea540772dc35306779b2d449593610c4eecb
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-11-07 12:49:35 +0100

    update scripts

--- build.sh
+++ build.sh
@@ -2,15 +2,20 @@
 
 set -e
 
-PUBLIC_KEY=$(cat .data/public_key)
-SECRET_KEY=$(cat .data/secret_key)
+RPC_URL="https://rpc-futurenet.stellar.org:443"
+NETWORK_PASS="Test SDF Future Network ; October 2022"
+FRIENDBOT_URL="https://friendbot-futurenet.stellar.org/"
+
+PUBLIC_KEY="GCSKTE64HQRTJR2F3I5HZYJSCEJMCDNMJTD77HCA3IO3NQBCIURU4SEE"
+SECRET_KEY="SC4GNDOETTVW7FCOKWZ4M7NND3M4XGALQ7OXXL43EP237QEUSHFXQVCG"
+
+echo -n "$PUBLIC_KEY" > .data/public_key
+echo -n "$SECRET_KEY" > .data/secret_key
+
+soroban config network add --rpc-url "$RPC_URL" --network-passphrase "$NETWORK_PASS" futurenet
 
 # build contracts
 soroban contract build
-
-RPC_URL="https://rpc-futurenet.stellar.org:443"
-
-NETWORK_PASS="Test SDF Future Network ; October 2022"
 
 # deploy contracts
 CONTRACT_ID=$(soroban contract deploy --wasm target/wasm32-unknown-unknown/release/my_contract.wasm --source $SECRET_KEY --rpc-url $RPC_URL --network-passphrase "${NETWORK_PASS}")
--- run.sh
+++ run.sh
@@ -2,13 +2,4 @@
 
 set -e
 
-PUBLIC_KEY=$(cat .data/public_key)
-SECRET_KEY=$(cat .data/secret_key)
-
-RPC_URL="https://rpc-futurenet.stellar.org:443"
-
-NETWORK_PASS="Test SDF Future Network ; October 2022"
-
-CONTRACT_ID=$(cat .data/contract_id)
-
-soroban contract invoke --id $CONTRACT_ID --source $SECRET_KEY --rpc-url $RPC_URL --network-passphrase "${NETWORK_PASS}" -- add --a 12 --b 3
+node js_side
