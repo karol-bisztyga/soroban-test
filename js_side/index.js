51a4840fe4a17780179fa899b3935e267494bbfb
Karol Bisztyga <karol.bisztyga@swmansion.com>
2023-11-07 13:32:57 +0100

    add trust map repro

--- js_side/index.js
+++ js_side/index.js
@@ -1,18 +1,11 @@
-const callContractModule = require('./call_contract');
+const { callContract, argParseInt, argParseMap, argParseString, argParseVec } = require('./call_contract');
 const fs = require('fs');
 
-console.log('hello',);
+const secretKey = fs.readFileSync('.data/secret_key').toString().replace(/\r?\n|\r/g, "");
+const contractId = fs.readFileSync('.data/contract_id').toString().replace(/\r?\n|\r/g, "");
 
-(async () => {
-  const secretKey = fs.readFileSync('.data/secret_key').toString().replace(/\r?\n|\r/g, "");
-  const contractId = fs.readFileSync('.data/contract_id').toString().replace(/\r?\n|\r/g, "");
-
-  const { callContract, argParseInt, argParseMap, argParseString } = callContractModule;
-
-  console.log(contractId, secretKey);
-
+const mapRepro = async () => {
   let result = null;
-
   // console.log('calling add');
   // result = await callContract(secretKey, contractId, 'add', argParseInt(6), argParseInt(7));
   // console.log('result of add', result);
@@ -57,4 +50,84 @@ console.log('hello',);
                    at ./runtime/asm_amd64.s:848
    */
 
+};
+
+const trustRepro = async () => {
+  await callContract(
+    secretKey,
+    contractId,
+    "clear_trust_map",
+  );
+
+  let data = fs.readFileSync('js_side/data/trust.json').toString();
+  data = JSON.parse(data);
+  let parsedResult = {};
+  for (let item of data.result) {
+    parsedResult[item.trusting_user._ref] = item.trusted_users.map((user) => user._ref);
+  }
+  console.log(parsedResult);
+
+  let counter = 0;
+  let size = Object.keys(parsedResult).length;
+  let limit = null; // 3;
+
+  /*
+  if you set a small limit (like 3), everything works, when you remove the limit, you get 23 invokations of set_trust_map_for_user_vec
+  get_trust_map throws this error:
+
+    {
+      status: 'FAILED',
+      latestLedger: '867294',
+      latestLedgerCloseTime: '1699360154',
+      oldestLedger: '865855',
+      oldestLedgerCloseTime: '1699352569'
+    }
+    An error has occured when calling a contract
+    - contract id: CA35SHZ2KG4B4ETP3D57PYBJNIH7DXTB3AB6FLWIV5D7RAJXYZ2RM76J
+    - function name: get_trust_map
+    - args: []
+    Error: FAILED
+        at innerCallContract (/Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/call_contract.js:56:13)
+        at async callContract (/Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/call_contract.js:87:12)
+        at async trustRepro (/Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/index.js:90:17)
+        at async /Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/index.js:100:3
+    node:internal/process/promises:288
+                triggerUncaughtException(err, true /* fromPromise * /);
+                ^
+
+    Error: FAILED
+        at innerCallContract (/Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/call_contract.js:56:13)
+        at async callContract (/Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/call_contract.js:87:12)
+        at async trustRepro (/Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/index.js:90:17)
+        at async /Users/karolbisztyga/Desktop/workspace/stellar/soroban-test/js_side/index.js:100:3
+  */
+
+  for (userId in parsedResult) {
+    ++counter;
+    console.log('----- calling set_trust_map_for_user_vec', counter, '/', size);
+    if (limit && counter > limit) {
+      console.log('limit reached, breaking');
+      break;
+    }
+    await callContract(
+      secretKey,
+      contractId,
+      "set_trust_map_for_user_vec",
+      argParseString(userId),
+      argParseVec(parsedResult[userId], argParseString),
+    );
+  }
+  console.log('setting trust map done, fetching back');
+
+  let fetched = await callContract(
+    secretKey,
+    contractId,
+    "get_trust_map",
+  );
+  console.log('fetched trust map', fetched);
+}
+
+(async () => {
+  // await mapRepro();
+  await trustRepro();
 })();
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 #![no_std]
-use soroban_sdk::{contract, contractimpl, Env, Map, String};
+use soroban_sdk::{contract, contractimpl, contracttype, Env, Map, String, Vec};
+
+#[derive(Clone)]
+#[contracttype]
+pub enum DataKey {
+  TrustMap,
+}
 
 #[contract]
 pub struct HelloContract;
@@ -16,6 +22,45 @@ impl HelloContract {
       result += value;
     }
     result
+  }
+
+  pub fn get_trust_map(env: Env) -> Map<String, Map<String, ()>> {
+    env
+      .storage()
+      .instance()
+      .get(&DataKey::TrustMap)
+      .unwrap_or(Map::new(&env))
+  }
+
+  pub fn set_trust_map(env: Env, trust_map: Map<String, Map<String, ()>>) {
+    env.storage().instance().set(&DataKey::TrustMap, &trust_map);
+  }
+
+  pub fn clear_trust_map(env: Env) {
+    env.storage().instance().set(
+      &DataKey::TrustMap,
+      &Map::<String, Map<String, ()>>::new(&env),
+    );
+  }
+
+  pub fn set_trust_map_for_user_vec(
+    env: Env,
+    user_id: String,
+    user_trust_map: Vec<String>,
+  ) -> Map<String, ()> {
+    let mut trust_map = HelloContract::get_trust_map(env.clone());
+
+    let mut new_map = Map::new(&env);
+    for item in user_trust_map {
+      new_map.set(item, ());
+    }
+
+    trust_map.set(user_id.clone(), new_map);
+
+    env.storage().instance().set(&DataKey::TrustMap, &trust_map);
+    HelloContract::get_trust_map(env.clone())
+      .get(user_id.clone())
+      .unwrap_or(Map::new(&env))
   }
 }
 
